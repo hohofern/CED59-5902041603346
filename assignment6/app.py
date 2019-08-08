51940a8fa8e5ace41f8ae91341356db0fc8f0b31
fern <fernza56@gmail.com>
2019-08-08 16:36:09 +0700

    assignment6

---
+++ assignment6/app.py
@@ -0,0 +1,15 @@
+from flask import Flask ,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   return render_template("Remember.html")
+
+@app.route('/' ,methods=["post"])
+def register():
+   return 'Welcome My friend'
+
+if __name__ == '__main__':
+    app.run()
