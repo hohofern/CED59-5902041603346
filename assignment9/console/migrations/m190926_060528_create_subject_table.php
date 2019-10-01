a5d1543491abb35dd1ecaa24deac8451914f5282
hohofern <fernza56@gmail.com>
2019-10-01 19:03:35 +0700

    add 9

---
+++ assignment9/console/migrations/m190926_060528_create_subject_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%subject}}`.
+ */
+class m190926_060528_create_subject_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%subject}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(256)->notNull(),
+            'section' => $this->string(),
+            'tech_id' => $this->integer(),
+            'detail' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%subject}}');
+    }
+}
